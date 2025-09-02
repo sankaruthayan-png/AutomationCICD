ea286c1fe6f9516a6c0e7914ba719d12cd9046c4
sankaruthayan-png <sankar.uthayan@sankaruthayan.com>
2025-09-02 10:15:29 -0400

    Update BaseTest.javaupdate

--- src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
+++ src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
@@ -1,6 +1,7 @@
 package rahulshettyacademy.TestComponents;
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,14 +19,11 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.BeforeMethod;
+// If you want Firefox/Edge, import their Options/Drivers similarly
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import rahulshettyacademy.pageobjects.LandingPage;
 
 public class BaseTest {
@@ -35,38 +33,43 @@ public class BaseTest {
 
     public WebDriver initializeDriver() throws IOException {
         Properties prop = new Properties();
-        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
-                + "//src//main//java//rahulshettyacademy//resources//GlobalData.properties");
+        FileInputStream fis = new FileInputStream(
+                System.getProperty("user.dir")
+                        + "//src//main//java//rahulshettyacademy//resources//GlobalData.properties");
         prop.load(fis);
 
-        String browserName = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
+        String browserName = System.getProperty("browser") != null
+                ? System.getProperty("browser")
+                : prop.getProperty("browser");
+        String browser = browserName == null ? "chrome" : browserName.toLowerCase();
 
-        if (browserName.contains("chrome")) {
+        if (browser.contains("chrome")) {
             ChromeOptions options = new ChromeOptions();
-            WebDriverManager.chromedriver().setup();
-            if (browserName.contains("headless")) {
-                options.addArguments("headless");
+
+            // Headless support: run as "chrome-headless" if needed
+            if (browser.contains("headless")) {
+                options.addArguments("--headless=new");
+                options.addArguments("--window-size=1440,900");
             }
+
+            // Selenium Manager (Selenium 4.6+) auto-resolves the right chromedriver for installed Chrome
             driver = new ChromeDriver(options);
-            driver.manage().window().setSize(new Dimension(1440, 900));
-        } else if (browserName.equalsIgnoreCase("firefox")) {
-            System.setProperty("webdriver.gecko.driver", "/Users/rahulshetty//documents//geckodriver");
-            driver = new FirefoxDriver();
-        } else if (browserName.equalsIgnoreCase("edge")) {
-            System.setProperty("webdriver.edge.driver", "edge.exe");
-            driver = new EdgeDriver();
+
+            if (!browser.contains("headless")) {
+                driver.manage().window().maximize();
+            }
+        } else {
+            throw new IllegalArgumentException("Only Chrome is configured in this BaseTest");
         }
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
         return driver;
     }
 
     public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException {
         String jsonContent = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
         ObjectMapper mapper = new ObjectMapper();
-        List<HashMap<String, String>> data = mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {});
-        return data;
+        return mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {});
     }
 
     public String getScreenshot(String testCaseName, WebDriver driver) throws IOException {
@@ -74,7 +77,7 @@ public class BaseTest {
         File source = ts.getScreenshotAs(OutputType.FILE);
         File file = new File(System.getProperty("user.dir") + "//reports//" + testCaseName + ".png");
         FileUtils.copyFile(source, file);
-        return System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
+        return file.getAbsolutePath();
     }
 
     @BeforeMethod(alwaysRun = true)
@@ -87,6 +90,8 @@ public class BaseTest {
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        driver.close();
+        if (driver != null) {
+            try { driver.quit(); } catch (Exception ignored) {}
+        }
     }
 }
