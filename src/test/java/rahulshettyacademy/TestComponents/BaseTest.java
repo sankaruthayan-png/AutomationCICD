17ff6fdc3ee222203f47e586c7c094c372d6709b
sankaruthayan-png <sankar.uthayan@sankaruthayan.com>
2025-09-02 10:06:36 -0400

    Update BaseTest.java for chrome browser version

--- src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
+++ src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
@@ -4,7 +4,6 @@ import org.testng.annotations.AfterMethod;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
@@ -21,7 +20,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -32,92 +30,63 @@ import rahulshettyacademy.pageobjects.LandingPage;
 
 public class BaseTest {
 
-	public WebDriver driver;
-	public LandingPage landingPage;
+    public WebDriver driver;
+    public LandingPage landingPage;
 
-	public WebDriver initializeDriver() throws IOException
+    public WebDriver initializeDriver() throws IOException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
+                + "//src//main//java//rahulshettyacademy//resources//GlobalData.properties");
+        prop.load(fis);
 
-	{
-		// properties class
+        String browserName = System.getProperty("browser") != null ? System.getProperty("browser") : prop.getProperty("browser");
 
-		 Properties prop = new Properties();
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
-				+ "//src//main//java//rahulshettyacademy//resources//GlobalData.properties");
-		prop.load(fis);
-		
-		String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") :prop.getProperty("browser");
-		//prop.getProperty("browser");
+        if (browserName.contains("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            WebDriverManager.chromedriver().setup();
+            if (browserName.contains("headless")) {
+                options.addArguments("headless");
+            }
+            driver = new ChromeDriver(options);
+            driver.manage().window().setSize(new Dimension(1440, 900));
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", "/Users/rahulshetty//documents//geckodriver");
+            driver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("edge")) {
+            System.setProperty("webdriver.edge.driver", "edge.exe");
+            driver = new EdgeDriver();
+        }
 
-		if (browserName.contains("chrome")) {
-			ChromeOptions options = new ChromeOptions();
-			WebDriverManager.chromedriver().setup();
-			if(browserName.contains("headless")){
-			options.addArguments("headless");
-			}		
-			driver = new ChromeDriver(options);
-			driver.manage().window().setSize(new Dimension(1440,900));//full screen
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        return driver;
+    }
 
-		} else if (browserName.equalsIgnoreCase("firefox")) {
-			System.setProperty("webdriver.gecko.driver",
-					"/Users/rahulshetty//documents//geckodriver");
-			driver = new FirefoxDriver();
-			// Firefox
-		} else if (browserName.equalsIgnoreCase("edge")) {
-			// Edge
-			System.setProperty("webdriver.edge.driver", "edge.exe");
-			driver = new EdgeDriver();
-		}
+    public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException {
+        String jsonContent = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
+        ObjectMapper mapper = new ObjectMapper();
+        List<HashMap<String, String>> data = mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {});
+        return data;
+    }
 
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.manage().window().maximize();
-		return driver;
+    public String getScreenshot(String testCaseName, WebDriver driver) throws IOException {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        File file = new File(System.getProperty("user.dir") + "//reports//" + testCaseName + ".png");
+        FileUtils.copyFile(source, file);
+        return System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
+    }
 
-	}
-	
-	public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException
-	{
-		//read json to string
-	String jsonContent = 	FileUtils.readFileToString(new File(filePath), 
-			StandardCharsets.UTF_8);
-	
-	//String to HashMap- Jackson Databind
-	
-	ObjectMapper mapper = new ObjectMapper();
-	  List<HashMap<String, String>> data = mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {
-      });
-	  return data;
-	
-	//{map, map}
+    @BeforeMethod(alwaysRun = true)
+    public LandingPage launchApplication() throws IOException {
+        driver = initializeDriver();
+        landingPage = new LandingPage(driver);
+        landingPage.goTo();
+        return landingPage;
+    }
 
-	}
-	
-	public String getScreenshot(String testCaseName,WebDriver driver) throws IOException
-	{
-		TakesScreenshot ts = (TakesScreenshot)driver;
-		File source = ts.getScreenshotAs(OutputType.FILE);
-		File file = new File(System.getProperty("user.dir") + "//reports//" + testCaseName + ".png");
-		FileUtils.copyFile(source, file);
-		return System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
-		
-		
-	}
-	
-	@BeforeMethod(alwaysRun=true)
-	public LandingPage launchApplication() throws IOException
-	{
-		
-		 driver = initializeDriver();
-		  landingPage = new LandingPage(driver);
-		landingPage.goTo();
-		return landingPage;
-	
-		
-	}
-	
-	@AfterMethod(alwaysRun=true)
-	
-	public void tearDown()
-	{
-		driver.close();
-	}
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.close();
+    }
 }
